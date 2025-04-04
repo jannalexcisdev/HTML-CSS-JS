c87ee3f0b7df6f60e846e90736aa04157f83fe5b
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-04-04 11:55:45 +0800

    17.2 inheritance

--- YT-SUPERSIMPLEDEV-JS/javascript-amazon-project/data/products.js
+++ YT-SUPERSIMPLEDEV-JS/javascript-amazon-project/data/products.js
@@ -34,7 +34,54 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
+}
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b: this.a
+// };
+
+// function logThis() {
+//   console.log(this);
+// }
+
+// logThis();
+// logThis.call('hello');
+
+// this
+// const object3 = {
+//   method: () => {
+//     console.log(this);
+//   }
+// };
+
+// object3.method();
 
 export const products = [
   {
@@ -696,5 +743,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
